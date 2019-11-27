5606ebdfad71bac25cf772d806bd73533562aa18
dench111 <46183310+dench111@users.noreply.github.com>
2019-11-27 21:50:44 +0300

    Update pipeline2.groovy

--- pipeline2.groovy
+++ pipeline2.groovy
@@ -1,10 +1,20 @@
 pipeline {
-    agent { docker { image 'python:3.5.1' } }
-    stages {
-        stage('build') {
-            steps {
-                sh 'python --version'
-            }
-        }
+  agent {
+    node {
+      label " "
     }
+  }
+  stages {
+    stage("Start monitoring script") {
+      steps {
+        script {
+          def workspace = "/var/lib/jenkins/workspace/Ansible_Test_WithGit"
+          echo "Проверка состояния сервера ELK"
+          sh "chmod ugo+rwx /var/lib/jenkins/workspace/Ansible_Test_WithGit/monitoring_script.py"
+          sh "pwd | ls -la"  
+          sh "/var/lib/jenkins/workspace/Ansible_Test_WithGit/monitoring_script.py"
+        }
+      }
+    }
+  }
 }
