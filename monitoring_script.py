f91a52fc7eb170c3ce40474b1ab159dca6ba3018
dench111 <46183310+dench111@users.noreply.github.com>
2019-11-27 23:28:34 +0300

    Update monitoring_script.py

--- monitoring_script.py
+++ monitoring_script.py
@@ -38,7 +38,7 @@ def checkResponseStatus(Resp, diskSpace):
         markers['unassignedMarker'] = 1
     if int(diskSpace) > 85:
         markers['diskSpaceMarker'] = 1
-    print(""\x1b[32mПИЗДЕЦ\x1b[0m")
+    print("\x1b[32mПИЗДЕЦ\x1b[0m")
 
  
 def markerCheck():
