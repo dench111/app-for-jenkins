b54921a333d858e4014357710af39bb000b834a6
dench111 <46183310+dench111@users.noreply.github.com>
2019-11-27 23:22:28 +0300

    Update pipeline2.groovy

--- pipeline2.groovy
+++ pipeline2.groovy
@@ -10,7 +10,6 @@ pipeline {
         script {
           def workspace = "/var/lib/jenkins/workspace/Ansible_Test_WithGit"
           echo "Проверка состояния сервера ELK"
-          sh "pip install termcolor"
           sh "chmod ugo+rwx /var/lib/jenkins/workspace/Ansible_Test_WithGit/monitoring_script.py"
           sh "/var/lib/jenkins/workspace/Ansible_Test_WithGit/monitoring_script.py"
         }
