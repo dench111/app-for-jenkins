afa29aff4499d81b345a87e4296c7baa4bb52bbf
dench111 <46183310+dench111@users.noreply.github.com>
2019-11-30 01:16:31 +0300

    добавил сброс env переменной

--- pipeline2.groovy
+++ pipeline2.groovy
@@ -13,6 +13,7 @@ pipeline {
           sh "chmod ugo+rwx /var/lib/jenkins/workspace/Ansible_Test_WithGit/monitoring_script.py"
           sh " chmod ugo+rwx /var/lib/jenkins/workspace/Ansible_Test_WithGit/sendemail.sh"
           sh "/var/lib/jenkins/workspace/Ansible_Test_WithGit/monitoring_script.py"
+          sh "unset ELKStatus"
           sh "rm -rf /var/lib/jenkins/workspace/Ansible_Test_WithGit/*"
         }
       }
