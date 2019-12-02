a0176f4ba7db2d6fa1138b2323b1f3fcb86c6217
dench111 <46183310+dench111@users.noreply.github.com>
2019-12-02 17:07:03 +0300

    Update monitoring_script.py

--- monitoring_script.py
+++ monitoring_script.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import print_function
-import json, sys, os, pprint, re, subprocess, io, platform, signal
+import json, sys, os, pprint, re, subprocess, io, platform, signal, time
 
 pingServer = 'ping -c 3 $ElasticURL'
 uptimeServer = 'uptime'
