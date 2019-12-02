be3ae9558d430ef3cb8cf3698fc5cd0163a9c9bc
dench111 <46183310+dench111@users.noreply.github.com>
2019-12-02 10:19:45 +0300

    Update pipeline2.groovy

--- pipeline2.groovy
+++ pipeline2.groovy
@@ -9,9 +9,7 @@ pipeline {
       steps {
         script {
           def workspace = "/var/lib/jenkins/workspace/Ansible_Test_WithGit"
-          echo "\033[32m Проверка состояния сервера ELK\n"
-          sh "chmod ugo+rwx /var/lib/jenkins/workspace/Ansible_Test_WithGit/monitoring_script.py"
-          sh " chmod ugo+rwx /var/lib/jenkins/workspace/Ansible_Test_WithGit/sendemail.sh"
+          sh "chmod ugo+rwx $workspace" && \
           sh "/var/lib/jenkins/workspace/Ansible_Test_WithGit/monitoring_script.py"
           sh "unset ELKStatus"
           sh "rm -rf /var/lib/jenkins/workspace/Ansible_Test_WithGit/*"
