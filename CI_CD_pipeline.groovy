4e211e99e23ca6161fe649a31b524341fad3ec4d
dench111 <46183310+dench111@users.noreply.github.com>
2020-05-22 16:37:30 +0300

    Update CI_CD_pipeline.groovy

--- CI_CD_pipeline.groovy
+++ CI_CD_pipeline.groovy
@@ -5,12 +5,12 @@ pipeline {
     }
   }
   stages {
-    stage("Start Ansible Playbook from Git") {
+    stage("download application sources from git to /opt/downloads") {
       steps {
         script {
           def workspace = "/var/jenkins_home/workspace/Pipeline_Job"
           sh "chmod ugo+rwx $workspace/*"
-          sh "ansible-playbook -i " + "inventory" + " " + "$workspace/CI_CD_playbook.yml"
+          sh "ansible-playbook -i " + "inventory" + " " + "$workspace/git_clone_repo.yml"
         }
       }
     }
