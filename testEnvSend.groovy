9652177d2e17f28604c666e81c3d12904c3ca5d1
dench111 <46183310+dench111@users.noreply.github.com>
2020-06-03 13:14:02 +0300

    Create testEnvSend.groovy

---
+++ testEnvSend.groovy
@@ -0,0 +1,25 @@
+#! groovy
+
+String FPname
+def workspace = "/var/jenkins_home/workspace/SendEnv"
+
+pipeline {
+  agent {
+    node {
+      label " "
+    }
+  }
+    stage("Get Python env") {
+      steps {
+        script {
+          sh "chmod ugo+rwx $workspace/scripts/*"
+          sh "/var/jenkins_home/workspace/SendEnv/scripts/envtest.py"
+          sh "echo $FPname"
+          sh "name = $FPname"
+          sh "echo $name"
+          sh "echo ${env.FPname}"
+        }
+      }
+    }
+  }
+ }
