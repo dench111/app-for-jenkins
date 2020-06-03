7c4aa3751f68780e73af3d7970103eb324ec229f
dench111 <46183310+dench111@users.noreply.github.com>
2020-06-03 09:29:18 +0300

    Update PomParser.py

--- scripts/PomParser.py
+++ scripts/PomParser.py
@@ -8,7 +8,7 @@ print(mypomcontent)
 mypomcontentregex = re.compile(r'(<finalName>)(\w+)')
 mo = mypomcontentregex.search(mypomcontent)
 print('FP_NAME: ' + mo.group(2))
-print($BUILD_NUMBER)
+print('$BUILD_NUMBER')
 #rename app
 appName = mo.group(2)
 fullAppName = appName + '-' + str($BUILD_NUMBER)
