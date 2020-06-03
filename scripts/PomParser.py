d7d1d573177abcc91ed11cb85286b7b804bfb7af
dench111 <46183310+dench111@users.noreply.github.com>
2020-06-03 14:51:08 +0300

    Update PomParser.py

--- scripts/PomParser.py
+++ scripts/PomParser.py
@@ -4,12 +4,11 @@ import os, re
 build_number = os.getenv('BUILD_NUMBER')
 mypom = open('/var/jenkins_home/workspace/sources/pom.xml')
 mypomcontent = mypom.read()
-print(mypomcontent)
 # find app name in pom.xml
 mypomcontentregex = re.compile(r'(<finalName>)(\w+)')
 mo = mypomcontentregex.search(mypomcontent)
-print('FP_NAME: ' + mo.group(2))
-print(build_number)
+#print('FP_NAME: ' + mo.group(2))
+#print(build_number)
 #rename app
 appName = mo.group(2)
 fullAppName = appName + '-' + str(build_number)
