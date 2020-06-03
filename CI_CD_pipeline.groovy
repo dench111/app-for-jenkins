e67d102785f0d448b3c259906f4ca9e3fc66b818
dench111 <46183310+dench111@users.noreply.github.com>
2020-06-03 17:17:14 +0300

    Update CI_CD_pipeline.groovy

--- CI_CD_pipeline.groovy
+++ CI_CD_pipeline.groovy
@@ -24,24 +24,14 @@ pipeline {
         }
       }
     }
-    stage("Naming distr") {
-      steps {
-        script {
-          sh "chmod ugo+rwx $workspace/scripts/*"
-          sh "/var/jenkins_home/workspace/Pipeline_Job/scripts/PomParser.py"
-          FPname='/var/jenkins_home/workspace/Pipeline_Job/scripts/PomParser.py'
-          sh "echo $FPname"
-        }
-      }
-    }
-    stage("Upload disrtibutiv to nexus") {
+    stage("Rename and Upload disrtibutiv to nexus") {
       steps {
        withCredentials([usernamePassword(credentialsId: '6deb43b4-4f40-425b-813a-6a21dc4e7c05',
                         usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
         script {
-          sh "echo $FPname"
-          ext=".jar"
-          sh "curl -v -u $USERNAME:$PASSWORD --upload-file /var/jenkins_home/workspace/sources/target/$FPname$ext http://192.168.0.84:8081/nexus/content/repositories/Testrep/$FPname$ext"
+          sh "chmod ugo+rwx $workspace/scripts/*"
+          sh "/var/jenkins_home/workspace/Pipeline_Job/scripts/PomParser.py"
+          //sh "curl -v -u $USERNAME:$PASSWORD --upload-file /var/jenkins_home/workspace/sources/target/$FPname$ext http://192.168.0.84:8081/nexus/content/repositories/Testrep/$FPname$ext"
         }
       }
     }
