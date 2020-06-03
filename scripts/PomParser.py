adbb33439e8c881725f45a5d9a9dac91de4f4e7c
dench111 <46183310+dench111@users.noreply.github.com>
2020-06-03 10:07:03 +0300

    Update PomParser.py

--- scripts/PomParser.py
+++ scripts/PomParser.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2
 import os, re
 # open pom.xml file
+build_number = os.getenv('BUILD_NUMBER')
 mypom = open('/var/jenkins_home/workspace/sources/pom.xml')
 mypomcontent = mypom.read()
 print(mypomcontent)
@@ -8,9 +9,9 @@ print(mypomcontent)
 mypomcontentregex = re.compile(r'(<finalName>)(\w+)')
 mo = mypomcontentregex.search(mypomcontent)
 print('FP_NAME: ' + mo.group(2))
-print(${env.BUILD_NUMBER})
+print(build_number)
 #rename app
 appName = mo.group(2)
-fullAppName = appName + '-' + str(${env.BUILD_NUMBER})
+fullAppName = appName + '-' + str(build_number)
 print(fullAppName)
 os.environ["FPname"] = str(fullAppName)
