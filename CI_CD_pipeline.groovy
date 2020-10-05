41cc6eeca6eb6d4c4fbf774fa972ccaafa5972ca
dench111 <46183310+dench111@users.noreply.github.com>
2020-10-05 22:45:50 +0300

    Update CI_CD_pipeline.groovy

--- CI_CD_pipeline.groovy
+++ CI_CD_pipeline.groovy
@@ -9,18 +9,26 @@ pipeline {
     }
   }
   stages {
-    stage("Download application sources from git") {
+    //выкачиваем исходники приложения на слейв jenkins
+    stage("Clone sources") {
       steps {
         script {
-          sh "ansible-playbook -i " + "inventory" + " " + "$workspace/Playbooks/git_clone_repo.yml"
-        }
-      }
+           // The below will clone your repo and will be checked out to master branch by default.
+           git credentialsId: 'fd7c264a-d14f-4d9f-94b6-ce610eb4ccbc', url: 'https://github.com/dench111/rest-service.git'
+           // Do a ls -lart to view all the files are cloned. It will be clonned. This is just for you to be sure about it.
+           sh "ls -lart ./*" 
+           // List all branches in your repo. 
+           sh "git branch -a"
+           // Checkout to a specific branch in your repo.
+           sh "git checkout master"
+          }
+       }
     }
     stage("Run build app with maven") {
       steps {
         script {
-          sh "mvn -f /var/jenkins_home/workspace/sources/ clean install"
-          sh "ls -la /var/jenkins_home/workspace/sources/target/"
+          sh "mvn -f /var/jenkins_home/workspace/Pipeline_Job/ clean install -Dmaven.test.skip=true"
+          sh "ls -la /var/jenkins_home/workspace/Pipeline_Job/target/"
         }
       }
     }
@@ -31,19 +39,17 @@ pipeline {
         script {
           sh "chmod ugo+rwx $workspace/scripts/*"
           sh "/var/jenkins_home/workspace/Pipeline_Job/scripts/PomParser.py"
-          //sh "curl -v -u $USERNAME:$PASSWORD --upload-file /var/jenkins_home/workspace/sources/target/$FPname$ext http://192.168.0.84:8081/nexus/content/repositories/Testrep/$FPname$ext"
+          //sh "curl -v -u $USERNAME:$PASSWORD --upload-file /var/jenkins_home/workspace/Pipeline_Job/target/$FPname$ext http://192.168.0.84:8081/nexus/content/repositories/Testrep/$FPname$ext"
         }
       }
     }
    }
-    stage("Clean source dir") {
-      steps {
-        script {
-          sh "rm -rf /var/jenkins_home/workspace/sources/*"
+    post {
+        always {
+            echo 'One way or another, I have finished'
+            deleteDir() /* clean up our workspace */
         }
-      }
     }
-  }
- }
+}
 
 
