9f6bd5e026cc5921fe543403c2b8e70b5da68a7c
dench111 <46183310+dench111@users.noreply.github.com>
2020-10-05 23:22:42 +0300

    Update PomParser.py

--- scripts/PomParser.py
+++ scripts/PomParser.py
@@ -4,13 +4,13 @@ import os, re
 build_number = os.getenv('BUILD_NUMBER')
 #username = os.getenv('USERNAME')
 #password = os.getenv('PASSWORD')
-request = 'curl -XPOST -v -u $USERNAME:$PASSWORD --upload-file "/var/jenkins_home/workspace/sources/target/$appNameold.jar" "http://192.168.0.84:8081/nexus/content/repositories/Testrep/$FPname.jar"'
+request = 'curl -XPOST -v -u $USERNAME:$PASSWORD --upload-file "/var/jenkins_home/workspace/target/$appNameold.jar" "http://192.168.0.84:8081/nexus/content/repositories/Testrep/$FPname.jar"'
 
 def uploadartifact(param):
         result = (os.popen(param).read())
         print(result)
 
-mypom = open('/var/jenkins_home/workspace/sources/pom.xml')
+mypom = open('/var/jenkins_home/workspace/Pipeline_Job/pom.xml')
 mypomcontent = mypom.read()
 # find app name in pom.xml
 mypomcontentregex = re.compile(r'(<finalName>)(\w+)')
